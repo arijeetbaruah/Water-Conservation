e8943ff2cd2e810b6dbaa34f16d93616713a0670
arijeetbaruah <arijeetbaruah@gmail.com>
2020-06-02 15:17:50 +0100

    shower

---
+++ Assets/Scripts/ShowerInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowerInteractable : Interactable
+{
+    public override void Interact(KeyCode key)
+    {
+        if (key == KeyCode.F)
+        {
+            LevelManager.manager.UseWater(5f);
+        }
+    }
+
+    public override void ShowOptions()
+    {
+        List<string> interactions = new List<string>();
+
+        interactions.Add("Take Shower");
+
+        GameManager.manager.ListInteraction(interactions);
+    }
+}
